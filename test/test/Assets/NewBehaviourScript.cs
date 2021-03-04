3c3e33fb9889e2cc7c9f713bb171cc4172e964cf
xp7353 <80015324+xp7353@users.noreply.github.com>
2021-03-04 10:23:04 +0900

    Initial commit

---
+++ test/test/Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    string animal = "양"; //문자열
+    int count = 0; //정수형
+
+    void Start()
+    {
+        Debug.Log(animal + "세기 프로그램");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log((++count) + "마리");
+    }
+}
---
+++ test/test/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private string name;
+    private int level = 1;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+
+    public void GetInfo()
+    {
+        Debug.Log("이름 : " + this.name);
+        Debug.Log("레벨 : " + this.level);
+    }
+
+    public void LevelUp()
+    {
+        this.level++;
+        Debug.Log("레벨업");
+    }
+}
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player myPlayer = new Player("wooseong");
+        myPlayer.GetInfo();
+        myPlayer.LevelUp();
+        myPlayer.GetInfo();
+    }
+}
